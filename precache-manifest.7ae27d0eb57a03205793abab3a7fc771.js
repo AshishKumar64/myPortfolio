bca1c4a3075580eb5e1865ff0bdd714c6afc5cc4
Ashish kumar <aakum98@gmail.com>
2021-06-09 10:54:55 +0530

    Updates

---
+++ precache-manifest.7ae27d0eb57a03205793abab3a7fc771.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b6344e9627a5a17fc15bf27fa06bb95",
+    "url": "/myPortfolio/index.html"
+  },
+  {
+    "revision": "479a28236f905a71d609",
+    "url": "/myPortfolio/static/css/main.7deb7b25.chunk.css"
+  },
+  {
+    "revision": "9b1d2c5a4130c09c43b1",
+    "url": "/myPortfolio/static/js/2.6955d607.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/myPortfolio/static/js/2.6955d607.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "479a28236f905a71d609",
+    "url": "/myPortfolio/static/js/main.e52638f1.chunk.js"
+  },
+  {
+    "revision": "0f713a185793586653f3",
+    "url": "/myPortfolio/static/js/runtime-main.d351083c.js"
+  },
+  {
+    "revision": "75c9b4e20d0655ec6511d453703914bd",
+    "url": "/myPortfolio/static/media/night.75c9b4e2.jpg"
+  },
+  {
+    "revision": "f80040d79e782f339bea8d967260b01b",
+    "url": "/myPortfolio/static/media/resume.f80040d7.pdf"
+  },
+  {
+    "revision": "7e846d427983bf0646219494fa384641",
+    "url": "/myPortfolio/static/media/sitting.7e846d42.png"
+  }
+]);
